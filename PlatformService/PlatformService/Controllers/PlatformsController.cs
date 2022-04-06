176e575d76d2077048441d8927697578d7dec5fe
Hamza Taoujouti <htaoujouti@integrationobjects.com>
2022-04-06 14:32:30 +0100

    fisrt commit imported from my platform service repos

---
+++ PlatformService/PlatformService/Controllers/PlatformsController.cs
@@ -0,0 +1,50 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using PlatformService.Data;
+using PlatformService.Dtos;
+using PlatformService.Models;
+using System;
+using System.Collections.Generic;
+
+namespace PlatformService.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PlatformsController : ControllerBase
+    {
+        private readonly IPlatformRepo _repo;
+        private readonly IMapper _mapper;
+
+        public PlatformsController(IPlatformRepo repo, IMapper mapper)
+        {
+            _mapper = mapper;
+            _repo = repo;
+        }
+        [HttpGet]
+        public ActionResult<IEnumerable<PlatformReadDto>> GetPlatforms()
+        {
+            Console.WriteLine("GetPlatforms()");
+            var PlatformItem = _repo.GetAllPlatforms();
+            return Ok(_mapper.Map<IEnumerable<PlatformReadDto>>(PlatformItem));
+        }
+        [HttpGet("{id}", Name = "GetPlatformById")]
+        public ActionResult<PlatformReadDto> GetPlatformById(int id)
+        {
+            Console.WriteLine("GetPlatformById(id)");
+            var platform = _repo.GetPlatformById(id);
+            if (platform != null)
+                return Ok(_mapper.Map<PlatformReadDto>(platform));
+            return NotFound();
+        }
+        [HttpPost]
+        public ActionResult<PlatformReadDto> CreatePlatform(PlatformCreateDto dto)
+        {
+            var platform = _mapper.Map<Platform>(dto);
+            _repo.CreatePlatform(platform);
+            _repo.SaveChanges();
+
+            var platformReadDto = _mapper.Map<PlatformReadDto>(platform);
+            return CreatedAtRoute(nameof(GetPlatformById), new { Id = platformReadDto.Id }, platformReadDto);
+        }
+    }
+}
