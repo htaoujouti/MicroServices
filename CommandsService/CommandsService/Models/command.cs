7d278faa887357d72d124d3fd1439dab6e841c09
Hamza Taoujouti <htaoujouti@integrationobjects.com>
2022-04-22 12:01:43 +0100

    add models into the commands service

---
+++ CommandsService/CommandsService/Models/command.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CommandsService.Models
+{
+    public class command
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string HowTo { get; set; }
+        [Required]
+        public string CommandLine { get; set; }
+        [Required]
+        public int PlatformId { get; set; }
+        public Platform platform { get; set; }
+    }
+}
