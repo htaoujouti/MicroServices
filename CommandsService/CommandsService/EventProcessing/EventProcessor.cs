50f3527297afecf0650048484b74277919b6efda
Hamza Taoujouti <htaoujouti@integrationobjects.com>
2022-05-04 15:52:09 +0100

    add construtor

--- CommandsService/CommandsService/EventProcessing/EventProcessor.cs
+++ CommandsService/CommandsService/EventProcessing/EventProcessor.cs
@@ -9,6 +9,20 @@ namespace CommandsService.EventProcessing
 {
     public class EventProcessor : IEventProcessor
     {
+        private readonly IServiceScopeFactory _scopefactory;
+
+        public IMapper _mapper { get; }
+
+        public EventProcessor(IServiceScopeFactory scopefactory, IMapper mapper)
+        {
+            _scopefactory = scopefactory;
+            _mapper = mapper;
+
+        }
+        public void ProcessEvent(string message)
+        {
+
+        }
         
 
     }
