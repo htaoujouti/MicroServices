f3b32886c6a1528c1297c54fd5942f720d75fc9e
Hamza Taoujouti <hamza.taoujouti@sofrecom.com>
2023-05-12 18:10:44 +0100

    adding profile to map grpc platforms to command platforms

--- CommandsService/CommandsService/Profiles/CommandsProfile.cs
+++ CommandsService/CommandsService/Profiles/CommandsProfile.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using CommandsService.Dtos;
 using CommandsService.Models;
+using PlatformService;
 
 namespace CommandsService.Profiles
 {
@@ -12,6 +13,10 @@ namespace CommandsService.Profiles
             CreateMap<Command , CommandReadDtos>();
             CreateMap<CommandCreateDtos, Command>();
             CreateMap<PlatformPublishedDto, Platform>().ForMember(dest => dest.ExternalId, opt => opt.MapFrom(src => src.Id));
+            CreateMap<GrpcPlatformModel, Platform>()
+                .ForMember(dest => dest.ExternalId, opt => opt.MapFrom(src => src.PlatformId))
+                .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
+                .ForMember(dest => dest.Commands, opt => opt.Ignore());
         }
     }
 }
